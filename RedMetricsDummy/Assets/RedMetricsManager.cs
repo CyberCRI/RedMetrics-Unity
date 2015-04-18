1708b918edb40e3aa1438ef68a7b2946ec8d5e48
raphael goujet <raphael.goujet@gmail.com>
2015-04-18 03:27:04 +0200

    first version, can create a player and an event

---
+++ RedMetricsDummy/Assets/RedMetricsManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RedMetricsManager : MonoBehaviour
+{
+		
+	void Start ()
+	{
+	}
+		
+	public IEnumerator GET (string url, System.Action<WWW> callback)
+	{
+		
+		WWW www = new WWW (url);
+		yield return www;
+		callback(www);
+	}
+		
+	public IEnumerator POST (string url, Dictionary<string,string> post, System.Action<WWW> callback)
+	{
+		WWWForm form = new WWWForm ();
+		foreach (KeyValuePair<string,string> post_arg in post) {
+			form.AddField (post_arg.Key, post_arg.Value);
+		}
+		
+		WWW www = new WWW (url, form);
+		yield return www;
+		callback(www);
+	}
+	
+	public IEnumerator POST (string url, byte[] post, Dictionary<string, string> headers, System.Action<WWW> callback)
+	{
+		WWW www = new WWW (url, post, headers);
+		yield return www;
+		callback(www);
+	}
+}
---
+++ RedMetricsDummy/Assets/RedMetricsUser.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class RedMetricsUser : MonoBehaviour {
+
+	public RedMetricsManager rmm; 
+
+	private string redMetricsURL = "https://api.redmetrics.io/v1/";
+	private string redMetricsEvent = "event";
+	private string redMetricsPlayer = "player";
+	private string gameVersion = "99a00e65-6039-41a3-a85b-360c4b30a466";
+	private string playerID = "b5ab445a-56c9-4c5b-a6d0-86e8a286cd81";
+
+	void Start () 
+	{
+		createEvent();
+	}
+
+	void setPlayerID(string pID)
+	{
+		playerID = pID;
+	}
+	
+	private void createEvent()
+	{
+		string url = redMetricsURL+redMetricsEvent;
+		Dictionary<string, string> headers = new Dictionary<string, string>();
+		headers.Add("Content-Type", "application/json");
+
+		string ourPostData = "{\"gameVersion\":\""+gameVersion+"\"," +
+			"\"player\":\""+playerID+"\"," +
+				"\"type\":\"todelete\"}";
+		byte[] pData = System.Text.Encoding.ASCII.GetBytes(ourPostData.ToCharArray());
+		WWW www;
+		StartCoroutine(rmm.POST(url, pData, headers, value => www = value));
+	}
+	
+	private void createPlayer()
+	{
+		string url = redMetricsURL+redMetricsPlayer;
+		Dictionary<string, string> headers = new Dictionary<string, string>();
+		headers.Add("Content-Type", "application/json");
+		string ourPostData = "{\"type\":todelete}";
+		byte[] pData = System.Text.Encoding.ASCII.GetBytes(ourPostData.ToCharArray());
+		WWW www;
+		StartCoroutine(rmm.POST(url, pData, headers, value => www = value));
+
+	}
+}
